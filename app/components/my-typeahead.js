c1706ac3ec961ef6034b70cba43d2ae6ed6f5c12
wei-ming <ztwo79@gmail.com>
2015-10-01 13:39:04 +0800

    when click outside of component  remove selected items

--- app/components/my-typeahead.js
+++ app/components/my-typeahead.js
@@ -4,7 +4,21 @@ export default Ember.Component.extend({
 	// suggest items
 	suggestItems:[],
 	selecedItems:[],
+	isSelecting:false,
+	focusOut:function() {
+		// not selecting any items
+		if(!this.get("isSelecting")){
+			this.set("suggestItems",[]);
+			this.set("searchWord","");
+		}
+	},
   	actions:{
+  		selecting:function(){
+  			this.set("isSelecting",true);
+  		},
+  		notSelecting:function(){
+  			this.set("isSelecting",false);	
+  		},
 		typing:function(){
       		// send this typeahead to action
       		this.sendAction('typing' , this);
@@ -14,6 +28,7 @@ export default Ember.Component.extend({
 			this.get("selecedItems").pushObject(suggestItem);
       		this.set("suggestItems",[]);
 			this.set("searchWord","");
+			this.set("isSelecting",false);	
 		},
 		// remove seleced item
 		removeSelectedItem:function(selecedItem){
